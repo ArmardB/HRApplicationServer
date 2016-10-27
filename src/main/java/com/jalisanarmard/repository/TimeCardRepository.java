9a377ea6320b7b6ae647a5676d54843dc3efa863
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-10-27 14:52:30 -0400

    Initial commit

---
+++ src/main/java/com/jalisanarmard/repository/TimeCardRepository.java
@@ -0,0 +1,7 @@
+package com.jalisanarmard.repository;
+
+import com.jalisanarmard.domain.TimeCard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TimeCardRepository extends CrudRepository<TimeCard, Long> {
+}
